ede0ebead425df2455a95c5b59f032becbed11a4
ncm-sina <18634330+ncm-sina@users.noreply.github.com>
2025-03-23 13:07:58 +0100

    removed some commented code

--- scripts/makeiso-docker.sh
+++ scripts/makeiso-docker.sh
@@ -4,7 +4,3 @@ cp ./dist/isodir ../docker/shared/ -fr
 
 /bin/sh ../docker/docker-make-iso.sh
 cp ../docker/shared/os0.iso ./dist/os0.iso
-
-# echo "will create iso from os files"
-# oscdimg -n -d -m -b./dist/isodir/boot/myos.bin -yo./scripts/orderfile.txt ./dist/isodir ./dist/myos-0.iso
-
